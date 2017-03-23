bfd0a41da32fef0f60ea09cd2e738ee4cb42a930
sakis tsalkitzis <sakis tsalkitzis>
2017-03-23 10:07:58 +0200

    Adding bootstrap and basepage

---
+++ src/main/java/com/sakis/BasePage.java
@@ -0,0 +1,13 @@
+package com.sakis;
+
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Created by sakis on 3/22/2017.
+ */
+public class BasePage extends WebPage {
+
+    public BasePage() {
+
+    }
+}
---
+++ src/main/java/com/sakis/ResultsPage.java
@@ -0,0 +1,7 @@
+package com.sakis;
+
+/**
+ * Created by sakis on 3/22/2017.
+ */
+public class ResultsPage {
+}
