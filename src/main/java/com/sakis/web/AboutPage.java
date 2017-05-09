3791cf69dc2299e6f7f205d86bf2a83ae2f6d968
Sakis Tsalkitzis <tsalk.sakis@gmail.com>
2017-05-09 21:13:42 +0300

    Adding About Page.

--- src/main/java/com/sakis/web/AboutPage.java
+++ src/main/java/com/sakis/web/AboutPage.java
@@ -1,16 +1,17 @@
 package com.sakis.web;
 
-import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
- * Created by sakis on 3/22/2017.
+ * Created by sakis on 5/9/2017.
  */
-public class    BasePage extends WebPage  {
+public class AboutPage extends BasePage {
 
+    public AboutPage(final PageParameters parameters) {
 
-
-    public BasePage() {
+        Form<?> form = new Form<Void>("form");
 
         Link homePageLink = new Link("homePage") {
             @Override
@@ -18,7 +19,7 @@ public class    BasePage extends WebPage  {
                 setResponsePage(HomePage.class);
             }
         };
-        add(homePageLink);
+        form.add(homePageLink);
 
         Link homePageLink2 = new Link("homePage2") {
             @Override
@@ -26,16 +27,7 @@ public class    BasePage extends WebPage  {
                 setResponsePage(HomePage.class);
             }
         };
-        add(homePageLink2);
-
-        Link aboutPageLink = new Link("aboutPage") {
-            @Override
-            public void onClick() {
-                setResponsePage(AboutPage.class);
-            }
-        };
-        add(aboutPageLink);
-
-
+        form.add(homePageLink2);
+        add(form);
     }
 }
