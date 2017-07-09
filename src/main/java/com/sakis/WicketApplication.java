315c5e58366418c3e980348c98d36287e1a7ccf5
Sakis Tsalkitzis <tsalk.sakis@gmail.com>
2017-07-09 15:00:07 +0300

    Changing uploading folder path, fixing collision bug. Changing inner URLs.

--- src/main/java/com/sakis/WicketApplication.java
+++ src/main/java/com/sakis/WicketApplication.java
@@ -1,6 +1,8 @@
 package com.sakis;
 
+import com.sakis.web.AboutPage;
 import com.sakis.web.HomePage;
+import com.sakis.web.ResultsPage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
@@ -8,7 +10,7 @@ import org.apache.wicket.protocol.http.WebApplication;
  * Application object for your web application.
  * If you want to run this application without deploying, run the Start class.
  * 
- * @see com.sakis.Start#main(String[])
+
  */
 public class WicketApplication extends WebApplication
 {
@@ -24,12 +26,26 @@ public class WicketApplication extends WebApplication
 	/**
 	 * @see org.apache.wicket.Application#init()
 	 */
+
+
+
+
+
 	@Override
 	public void init()
+
 	{
+
+
 
 		getDebugSettings().setAjaxDebugModeEnabled(false);
 		super.init();
+
+		mountPage("Results", ResultsPage.class);
+		mountPage("About", AboutPage.class);
+
+		mountPage("Home", HomePage.class);
+
 
 		// add your configuration here
 	}
--- src/main/java/com/sakis/web/HomePage.java
+++ src/main/java/com/sakis/web/HomePage.java
@@ -1,6 +1,7 @@
 package com.sakis.web;
 
 import  org.apache.commons.io.FilenameUtils;
+import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.IAjaxIndicatorAware;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
@@ -11,66 +12,82 @@ import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.file.File;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.nio.file.Paths;
 
 
 public class HomePage extends BasePage implements IAjaxIndicatorAware {
 
     private FileUploadField fileUpload;
-    private String UPLOAD_FOLDER = Paths.get(".").toAbsolutePath().toString();;
+
+    private String UPLOAD_FOLDER = "C:\\PomUploads\\";
 
 
-	public HomePage(final PageParameters parameters) {
+    public HomePage(final PageParameters parameters) {
 
 
-
-       final FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
+        final FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
 
         add(feedbackPanel);
 
         feedbackPanel.setOutputMarkupId(true);
 
-         Form<?> form = new Form<Void>("form");
+        Form<?> form = new Form<Void>("form");
 
 
         AjaxButton ab = new AjaxButton("submit") {
             protected void onSubmit(AjaxRequestTarget target, Form form) {
                 target.add(feedbackPanel);
-                if (target!=null) {
-                   final FileUpload uploadedFile = fileUpload.getFileUpload();
-                   if (uploadedFile != null) {
+                if (target != null) {
+                    final FileUpload uploadedFile = fileUpload.getFileUpload();
+                    if (uploadedFile != null) {
 
-                     String extension = FilenameUtils.getExtension(uploadedFile.getClientFileName());
-                     String name = uploadedFile.getClientFileName();
+                        String extension = FilenameUtils.getExtension(uploadedFile.getClientFileName());
+                        String name = uploadedFile.getClientFileName();
 
-                     if (name.equals("pom.xml") && (extension.equals("xml"))) {
+                        if (name.equals("pom.xml") && (extension.equals("xml"))) {
 
-                           File newFile = new File(UPLOAD_FOLDER
-                                 + uploadedFile.getClientFileName());
-                         if (newFile.exists()) {
-                             newFile.delete();}
+                            int usercount =0;
 
-                         try {
-                             newFile.createNewFile();
-                             uploadedFile.writeTo(newFile);
-                         } catch (Exception e) {
-                             throw new IllegalStateException("Error");}
+                           usercount = ChangeUserCount(UPLOAD_FOLDER,usercount);
 
-                         PageParameters pageParameters = new PageParameters();
+                            System.out.println(usercount);
 
-                         pageParameters.add("filename", UPLOAD_FOLDER
-                                 + uploadedFile.getClientFileName());
+                            File dir = new File(UPLOAD_FOLDER + usercount);
+                            dir.mkdir();
 
-                         pageParameters.add("uploadfolder", UPLOAD_FOLDER);
+                            File newFile = new File(UPLOAD_FOLDER + usercount +"\\"
+                                    + uploadedFile.getClientFileName());
+                            if (newFile.exists()) {
+                                newFile.delete();
+                            }
 
-                         setResponsePage(ResultsPage.class, pageParameters);
-                     } else
-                         info("Only pom.xml files are accepted");
-                 }
-             }
+                            try {
+                                newFile.createNewFile();
+                                uploadedFile.writeTo(newFile);
+                            } catch (Exception e) {
+                                throw new IllegalStateException("Error");
+                            }
+
+                            PageParameters pageParameters = new PageParameters();
+
+                            pageParameters.add("filename", UPLOAD_FOLDER + usercount +"\\"
+                                    + uploadedFile.getClientFileName());
+
+                            pageParameters.add("uploadfolder", UPLOAD_FOLDER + usercount +"\\");
+
+
+                            setResponsePage(ResultsPage.class, pageParameters);
+
+                        } else
+                            info("Only pom.xml files are accepted");
+                    }
+                }
             }
         };
-
 
 
         form.add(ab);
@@ -85,9 +102,7 @@ public class HomePage extends BasePage implements IAjaxIndicatorAware {
         form.add(fileUpload);
 
 
-
         add(form);
-
 
 
     }
@@ -97,4 +112,42 @@ public class HomePage extends BasePage implements IAjaxIndicatorAware {
     public String getAjaxIndicatorMarkupId() {
         return "loadingIndicator_id";
     }
+
+
+    public int ChangeUserCount(String folder, int  usercount) {
+
+
+        try {
+            BufferedReader file = new BufferedReader(new FileReader(folder+"users.txt"));
+            String number;
+            StringBuffer inputBuffer = new StringBuffer();
+
+            if ((number = file.readLine()) != null) {
+                inputBuffer.append(number);
+
+            }
+
+            file.close();
+
+
+            String inputStr = inputBuffer.toString();
+
+            usercount = Integer.parseInt(number);
+
+             int newcount = usercount + 1;
+             inputStr =  String.valueOf(newcount);
+
+            FileOutputStream fileOut = new FileOutputStream(folder+"users.txt");
+            fileOut.write(inputStr.getBytes());
+            fileOut.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+           return  usercount;
+
+
+    }
+
 }
